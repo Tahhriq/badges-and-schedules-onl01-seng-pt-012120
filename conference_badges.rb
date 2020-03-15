f85c38fe56b55abc3f4d571ec558245dff6ad81d
Tariq M Sulaiman <tariqsulaiman11@gmail.com>
2020-03-15 06:35:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,27 @@
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  badges = []
-  speakers.each do |speaker|
-    speaker.push{HelloASA
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+
+end
+
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
